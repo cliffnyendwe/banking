fe6923a80e99666900578f9ace5380af2628e9a7
cliffnyendwe <cliffnyendwe2018@gmail.com>
2025-03-26 17:11:45 +0300

    added environment

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5473;
 
 // Default route to prevent 404 errors on root
 app.get("/", (req, res) => {
